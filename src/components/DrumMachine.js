9960a1b0f7ab1abe8a19e02d21e5c983520b4a87
unknown <carlos.velazquez91@gmail.com>
2022-09-30 14:58:53 -0400

    Addd sound play action to pad useefect

--- src/components/DrumMachine.js
+++ src/components/DrumMachine.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Col, Container, Form, Row } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFreeCodeCamp } from '@fortawesome/free-brands-svg-icons';
@@ -8,13 +8,20 @@ import { bankOne, bankTwo } from '../utils/constants';
 const DrumMachine = () => {
   const [power, setPower] = useState(true);
   const [currentPadBank, setCurrentPadBank] = useState(bankOne);
+  const [volume, SetVolume] = useState(0.5);
+  const [currentPadId, setCurrentPadId] = useState('');
 
   const changePower = () => {
     setPower((current) => !current);
+    setCurrentPadId('');
   };
 
   const changeBank = () => {
     setCurrentPadBank((current) => (current === bankOne ? bankTwo : bankOne));
+  };
+
+  const volumeChangeHandler = (e) => {
+    SetVolume(e.target.value);
   };
 
   const powerSlider = power
@@ -34,16 +41,44 @@ const DrumMachine = () => {
           float: 'left',
         };
 
-  const Pads = () => {
+  const playAudio = ({ audio, id }) => {
+    if (power) {
+      setCurrentPadId(id);
+      const audioToPlay = new Audio(audio);
+      audioToPlay.volume = volume;
+      audioToPlay.play();
+    }
+  };
+
+  const Pad = ({ playHandler, pad }) => {
+    const [active, setActive] = useState(false);
+
+    useEffect(() => {
+      if (active) {
+        playHandler({ audio: pad.url, id: pad.id });
+        setTimeout(() => setActive(false), 100);
+      }
+    }, [active, playHandler, pad]);
+
+    const clickHandler = () => {
+      setActive(true);
+    };
+
     return (
-      <>
-        {currentPadBank.map((pad) => (
-          <div key={pad.id} className='key inactiveStyle text-center'>
-            <audio className='clip' id={pad.keyTrigger} src={pad.url} />
-            {pad.keyTrigger}
-          </div>
-        ))}
-      </>
+      <div
+        className={`drum-pad ${
+          active ? 'activeStyle' : 'inactiveStyle'
+        } text-center`}
+        onClick={() => clickHandler()}
+      >
+        <audio
+          className='clip'
+          id={pad.keyTrigger}
+          src={pad.url}
+          volume={volume}
+        />
+        {pad.keyTrigger}
+      </div>
     );
   };
 
@@ -72,8 +107,10 @@ const DrumMachine = () => {
             </p>
             <Row>
               <Col xs={12} md={6}>
-                <div className='keys-container'>
-                  <Pads />
+                <div className='drum-pad-container'>
+                  {currentPadBank.map((pad) => (
+                    <Pad key={pad.id} playHandler={playAudio} pad={pad} />
+                  ))}
                 </div>
               </Col>
               <Col xs={12} md={6}>
@@ -84,9 +121,17 @@ const DrumMachine = () => {
                       <div className='inner-select' style={powerSlider} />
                     </div>
                   </div>
-                  <p id='display'></p>
+                  <p id='display' className='text-center'>
+                    {currentPadId}
+                  </p>
                   <div className='volume-slider'>
-                    <Form.Range max={1} min={0} step={0.01} />
+                    <Form.Range
+                      max={1}
+                      min={0}
+                      step={0.01}
+                      value={volume}
+                      onChange={volumeChangeHandler}
+                    />
                   </div>
                   <div className='control'>
                     <p className='text-center'>Bank</p>
