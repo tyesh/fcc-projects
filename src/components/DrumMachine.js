769a5b10ca697807769f8bf59362a8bf7afdd837
unknown <carlos.velazquez91@gmail.com>
2022-10-01 10:45:41 -0400

    Removed previus version of drum machine component

--- src/components/DrumMachine.js
+++ src/components/DrumMachine.js
@@ -1,16 +1,16 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { Col, Container, Form, Row } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFreeCodeCamp } from '@fortawesome/free-brands-svg-icons';
 import bgImage from '../resources/bg/drummachinebg.jpg';
 import { bankOne, bankTwo } from '../utils/constants';
+import DrumPad from './DrumPad';
 
 const DrumMachine = () => {
   const [power, setPower] = useState(true);
   const [currentPadBank, setCurrentPadBank] = useState(bankOne);
   const [volume, SetVolume] = useState(0.5);
   const [currentPadId, setCurrentPadId] = useState('');
-  const [currentKey, setCurrentKey] = useState(false);
 
   const changePower = () => {
     setPower((current) => !current);
@@ -42,74 +42,8 @@ const DrumMachine = () => {
           float: 'left',
         };
 
-  const keyUpHandler = (event) => {
-    const { keyCode } = event;
-    setCurrentKey(keyCode);
-  };
-
-  useEffect(() => {
-    document.addEventListener('keydown', keyUpHandler, false);
-  }, []);
-
-  const playAudio = ({ audio, id }) => {
-    if (power) {
-      setCurrentPadId(id);
-      const audioToPlay = new Audio(audio);
-      audioToPlay.volume = volume;
-      audioToPlay.play();
-    }
-  };
-
   const updateDisplay = (padId) => {
     setCurrentPadId(padId.replace(/-/g, ' '));
-  };
-
-  const Pad = ({ currentKey, pad, playHandler, updateDisplay }) => {
-    const [active, setActive] = useState(false);
-    const [key, setKey] = useState(currentKey);
-
-    useEffect(() => {
-      if (key && key === pad.keyCode) {
-        setActive(true);
-      }
-    }, [key, pad]);
-
-    useEffect(() => {
-      if (active) {
-        //playHandler({ audio: pad.url, id: pad.id });
-        updateDisplay(pad.id);
-        const sound = document.getElementById(pad.keyTrigger);
-        sound.currentTime = 0;
-        sound.play();
-        setTimeout(() => {
-          setCurrentKey(false);
-          setActive(false);
-        }, 100);
-        setKey(false);
-      }
-    }, [active, playHandler, pad, updateDisplay]);
-
-    const clickHandler = () => {
-      setActive(true);
-    };
-
-    return (
-      <div
-        className={`drum-pad ${
-          active ? 'activeStyle' : 'inactiveStyle'
-        } text-center`}
-        id={pad.id}
-        onClick={() => clickHandler()}
-      >
-        <audio
-          className='clip'
-          id={pad.keyTrigger}
-          src={pad.url}
-          volume={volume}
-        />
-        {pad.keyTrigger}
-      </div>
-    );
   };
 
   return (
@@ -139,12 +73,15 @@ const DrumMachine = () => {
               <Col xs={12} md={6}>
                 <div className='drum-pad-container'>
                   {currentPadBank.map((pad) => (
-                    <Pad
+                    <DrumPad
                       key={pad.id}
-                      playHandler={playAudio}
-                      pad={pad}
-                      currentKey={currentKey}
+                      clip={pad.url}
+                      clipId={pad.id}
+                      keyCode={pad.keyCode}
+                      keyTrigger={pad.keyTrigger}
+                      power={power}
                       updateDisplay={updateDisplay}
+                      volume={volume}
                     />
                   ))}
                 </div>
