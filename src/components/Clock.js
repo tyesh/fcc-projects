1519020f30b76fe2ec3b6bd6e088235700a257f0
unknown <carlos.velazquez91@gmail.com>
2022-10-13 16:51:41 -0300

    Changes for clock layout

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -1,7 +1,13 @@
 import React from 'react';
-import { Col, Container, Row } from 'react-bootstrap';
+import { Card, Col, Container, Row } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faArrowUp, faArrowDown } from '@fortawesome/free-solid-svg-icons';
+import {
+  faArrowUp,
+  faArrowDown,
+  faPlay,
+  faPause,
+  faRotateRight,
+} from '@fortawesome/free-solid-svg-icons';
 import bgImage from '../resources/bg/clockbg.jpg';
 
 const Clock = () => {
@@ -19,59 +25,85 @@ const Clock = () => {
             minHeight: '100vh',
           }}
         >
-          <Col sm={12} md={6} className='clock-layout'>
-            <h1
-              className='text-center p-3'
-              style={{ color: '#fff', lineHeight: '40px', fontSize: '42px' }}
-            >
-              25 + 5 Clock
-            </h1>
-            <Row>
-              <Col
-                sm={6}
-                className='d-flex flex-row justify-content-center align-items-center'
-                style={{ marginBottom: 0 }}
-              >
-                <FontAwesomeIcon
-                  icon={faArrowDown}
-                  color='#000'
-                  className='arrowBtn'
-                  //onClick={() => setEditorMax(!editorMax)}
-                />
-                <p style={{ margin: '0 10px' }}>Break Length</p>
-                <FontAwesomeIcon
-                  icon={faArrowUp}
-                  color='#000'
-                  className='arrowBtn'
-                  //onClick={() => setEditorMax(!editorMax)}
-                />
-              </Col>
-              <Col
-                sm={6}
-                className='d-flex flex-row justify-content-center align-items-center'
-                style={{ marginBottom: 0 }}
-              >
-                <FontAwesomeIcon
-                  icon={faArrowDown}
-                  color='#000'
-                  className='arrowBtn'
-                  //onClick={() => setEditorMax(!editorMax)}
-                />
-                <p style={{ margin: '0 10px' }}>Session Length</p>
-                <FontAwesomeIcon
-                  icon={faArrowUp}
-                  color='#000'
-                  className='arrowBtn'
-                  //onClick={() => setEditorMax(!editorMax)}
-                />
-              </Col>
-            </Row>
-            <Row className='justify-content-center'>
-              <Col xs={6} className='timer text-center'>
-                Session
-                <p className='label'>25:00</p>
-              </Col>
-            </Row>
+          <Col xs={6}>
+            <Card>
+              <Card.Header>
+                <h1 className='text-center'>25 + 5 Clock</h1>
+              </Card.Header>
+              <Card.Body>
+                <Row>
+                  <Col
+                    sm={6}
+                    className='d-flex flex-row justify-content-center align-items-center'
+                    style={{ marginBottom: 0 }}
+                  >
+                    <FontAwesomeIcon
+                      icon={faArrowDown}
+                      color='#000'
+                      className='arrowBtn'
+                      //onClick={() => setEditorMax(!editorMax)}
+                    />
+                    <p style={{ margin: '0' }}>Break Length</p>
+                    <FontAwesomeIcon
+                      icon={faArrowUp}
+                      color='#000'
+                      className='arrowBtn'
+                      //onClick={() => setEditorMax(!editorMax)}
+                    />
+                  </Col>
+                  <Col
+                    sm={6}
+                    className='d-flex flex-row justify-content-center align-items-center'
+                    style={{ marginBottom: 0 }}
+                  >
+                    <FontAwesomeIcon
+                      icon={faArrowDown}
+                      color='#000'
+                      className='arrowBtn'
+                      //onClick={() => setEditorMax(!editorMax)}
+                    />
+                    <p style={{ margin: '0' }}>Session Length</p>
+                    <FontAwesomeIcon
+                      icon={faArrowUp}
+                      color='#000'
+                      className='arrowBtn'
+                      //onClick={() => setEditorMax(!editorMax)}
+                    />
+                  </Col>
+                </Row>
+                <Row className='justify-content-center'>
+                  <Col xs={6} className='timer text-center'>
+                    <h2>Session</h2>
+                    <p className='label'>25:00</p>
+                  </Col>
+                </Row>
+                <Row className='justify-content-center'>
+                  <Col xs={6} className='d-flex justify-content-center my-3'>
+                    <FontAwesomeIcon
+                      icon={faPlay}
+                      color='#000'
+                      className='arrowBtn'
+                      size='2x'
+                      //onClick={() => setEditorMax(!editorMax)}
+                    />
+                    <FontAwesomeIcon
+                      icon={faPause}
+                      color='#000'
+                      className='arrowBtn'
+                      size='2x'
+                      //onClick={() => setEditorMax(!editorMax)}
+                    />
+                    <FontAwesomeIcon
+                      icon={faRotateRight}
+                      color='#000'
+                      className='arrowBtn'
+                      size='2x'
+                      //onClick={() => setEditorMax(!editorMax)}
+                    />
+                  </Col>
+                </Row>
+              </Card.Body>
+            </Card>
           </Col>
         </Row>
       </Container>
