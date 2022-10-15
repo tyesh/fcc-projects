2c8d6bfa632f2c64f7f9a716a2ddddb82e73a119
unknown <carlos.velazquez91@gmail.com>
2022-10-15 09:35:31 -0300

    Added timer in useeffect

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Card, Col, Container, Row } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
@@ -13,6 +13,47 @@ import bgImage from '../resources/bg/clockbg.jpg';
 const Clock = () => {
   const [breackLength, setBreakLength] = useState(5);
   const [sessionLenth, setSessionLength] = useState(25);
+  const [timeLeft, setTimeLeft] = useState({ minutes: 25, seconds: 0 });
+  const [clockStatus, setClockStatus] = useState({
+    isPlaying: false,
+    currentTimer: 'session',
+  });
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTimeLeft((current) => {
+        if (clockStatus.isPlaying) {
+          let mins = current.minutes;
+          let secs = current.seconds;
+          if (secs === 0) {
+            secs = 59;
+            mins = mins - 1;
+          } else {
+            secs = secs - 1;
+          }
+          if (mins < 0) {
+          }
+          return { minutes: mins, seconds: secs };
+        } else {
+          return current;
+        }
+      });
+    }, 1000);
+    return () => clearInterval(interval);
+  }, [clockStatus]);
+
+  const resetHandler = () => {
+    setBreakLength(5);
+    setSessionLength(25);
+    setTimeLeft('25:00');
+  };
+
+  const formatTime = (time) => {
+    if (time < 10) {
+      return '0' + time;
+    }
+    return time;
+  };
 
   return (
     <main
@@ -65,12 +106,13 @@ const Clock = () => {
                       color='#000'
                       className='arrowBtn'
                       id='break-decrement'
+                      size='2x'
                       onClick={() =>
                         setBreakLength((current) => {
-                          if (current > 0) {
+                          if (current > 1) {
                             return current - 1;
                           }
-                          return 0;
+                          return 1;
                         })
                       }
                     />
@@ -82,6 +124,7 @@ const Clock = () => {
                       color='#000'
                       className='arrowBtn'
                       id='break-increment'
+                      size='2x'
                       onClick={() =>
                         setBreakLength((current) => {
                           if (current < 60) {
@@ -102,12 +145,13 @@ const Clock = () => {
                       color='#000'
                       className='arrowBtn'
                       id='session-decrement'
+                      size='2x'
                       onClick={() =>
                         setSessionLength((current) => {
-                          if (current > 0) {
+                          if (current > 1) {
                             return current - 1;
                           }
-                          return 0;
+                          return 1;
                         })
                       }
                     />
@@ -123,6 +167,7 @@ const Clock = () => {
                       color='#000'
                       className='arrowBtn'
                       id='session-increment'
+                      size='2x'
                       onClick={() =>
                         setSessionLength((current) => {
                           if (current < 60) {
@@ -136,34 +181,47 @@ const Clock = () => {
                 </Row>
                 <Row className='justify-content-center'>
                   <Col xs={6} className='timer text-center'>
-                    <h2 id='timer-label'>Session</h2>
-                    <p className='h3' id='time-left'>
-                      25:00
+                    <p id='timer-label' className='h3'>
+                      Session
+                    </p>
+                    <p className='h1' id='time-left'>
+                      {`${formatTime(timeLeft.minutes)}:${formatTime(
+                        timeLeft.seconds
+                      )}`}
                     </p>
                   </Col>
                 </Row>
                 <Row className='justify-content-center'>
                   <Col xs={6} className='d-flex justify-content-center my-3'>
                     <FontAwesomeIcon
-                      icon={faPlay}
+                      icon={clockStatus.isPlaying ? faPause : faPlay}
                       color='#000'
                       className='arrowBtn'
                       size='2x'
-                      //onClick={() => setEditorMax(!editorMax)}
+                      id='start_stop'
+                      onClick={() =>
+                        setClockStatus((current) => {
+                          return {
+                            isPlaying: !current.isPlaying,
+                            currentTimer: current.currentTimer,
+                          };
+                        })
+                      }
                     />
                     <FontAwesomeIcon
                       icon={faPause}
                       color='#000'
                       className='arrowBtn'
                       size='2x'
-                      //onClick={() => setEditorMax(!editorMax)}
+                      onClick={() => resetHandler()}
                     />
                     <FontAwesomeIcon
                       icon={faRotateRight}
                       color='#000'
                       className='arrowBtn'
                       size='2x'
-                      //onClick={() => setEditorMax(!editorMax)}
+                      id='reset'
+                      onClick={() => resetHandler()}
                     />
                   </Col>
                 </Row>
